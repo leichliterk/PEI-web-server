4363f5978eb18111081710d35b2216331f6742d0
leichliterk@gmail.com <leichliterk@gmail.com>
2025-06-11 20:56:03 -0500

    Update db config

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 // const uri = MONGODB_URI = `mongodb+srv://api_user:fkv2ygk3HTE@xbn8zxr@cluster0.zf8h7c3.mongodb.net/${process.env.STAGING ? "staging" : "production"}`;
-const uri = `mongodb+srv://api_user:fkv2ygk3HTE@xbn8zxr@cluster0.zf8h7c3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://api_user:5ZKBbw9YBgFKb4uz@cluster0.zf8h7c3.mongodb.net/${process.env.STAGING ? "staging" : "production"}`;
 
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
