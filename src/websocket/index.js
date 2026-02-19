1f6b3ce716845078563198eec6eff585954fe3c4
Kent Leichliter <leichliterk@gmail.com>
2026-02-18 21:24:38 -0600

    More logging

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -32,6 +32,10 @@ function initializeWebSocket(httpServer) {
     desktopNamespace.on('connection', (socket) => {
         connectionHandler.onConnect(socket, desktopNamespace);
 
+        socket.onAny((event, ...args) => {
+            console.log(`[Desktop socket] Event received: "${event}"`);
+        });
+
         socket.on('heartbeat', (data) => heartbeatHandler.onHeartbeat(socket, data));
         socket.on('status_update', (data) => connectionHandler.onStatusUpdate(socket, data));
         socket.on('file_upload', (data) => fileHandler.onFileUpload(socket, data));
